e214f548ea7976ab83ebf0482c634621ebcf701e
AngelPhenix <breathstylebro@gmail.com>
2024-01-14 00:51:44 +0100

    Changed player type to kinematic for easier movements, added vars related to it

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,12 @@
-extends Node2D
+extends KinematicBody2D
 
 var hp_max: int = 250
 var inventory: Array = []
 var experience = 20
 var mp_max: int = 50
+
+var speed: int = 80
+var velocity: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
